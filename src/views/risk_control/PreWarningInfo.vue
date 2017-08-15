46964599d4959d8107d182792d2e076b57e30732
chengzhiyuan <chengzhiyuan@ktjr.com>
2017-08-15 15:36:50 +0800

    程志远修改风控模块bugs

--- src/views/risk_control/PreWarningInfo.vue
+++ src/views/risk_control/PreWarningInfo.vue
@@ -8,26 +8,35 @@
           el-input(placeholder='预警名称', icon='search', @keyup.native.13="search", v-model='filter.name')
           el-select(v-model="filter.status",filterable,placeholder="状态",@change="search",)
             el-option(v-for="option in options",:key="option.value",:value="option.value",:label="option.label")
-          el-input(placeholder='对象ID', icon='search', @keyup.native.13="search", v-model='filter.subjectId')
+          el-input(placeholder='预警对象ID', icon='search', @keyup.native.13="search", v-model='filter.subjectId')
           el-button(size="small", type="primary",@click="search") 查找
           el-button(size="small", type="primary",@click="clearFilter")  清除
     .risk-table.table-container
       el-table(:data="riskDatas")
-        el-table-column(prop="assetFrom",label="资产方")
-        //- el-table-column(prop="riskRuleId",label="规则ID")
-        el-table-column(prop="name",label="预警名称")
-        el-table-column(prop="id",label="ID")
-        el-table-column(prop="subjectId",label="预警对象ID")
-        el-table-column(prop="status",label="当前状态")
+        el-table-column(label="资产方")
           template(scope="scope")
-            span {{scope.row.status | riskState}}
-        el-table-column(prop="createdAt",label="预警创建时间",width="150")
+            span {{scope.row.assetFrom | ktNull}}
+        el-table-column(label="预警名称",width="180")
+          template(scope="scope")
+            span {{scope.row.name | ktNull}}
+        //- el-table-column(label="ID")
+        //-   template(scope="scope")
+        //-     span {{scope.row.id | ktNull}}
+        el-table-column(label="预警对象ID",width="250")
+          template(scope="scope")
+            span {{scope.row.subjectId | ktNull}}
+        el-table-column(label="当前状态")
+          template(scope="scope")
+            span {{scope.row.status | riskWarn}}
+        el-table-column(label="预警创建时间",width="150")
+          template(scope="scope")
+            span {{scope.row.createdAt | moment}}
+        el-table-column(label="预警更新时间",width="150")
           template(scope="scope")
             span {{scope.row.updatedAt | moment}}
-        el-table-column(prop="updatedAt",label="预警更新时间",width="150")
+        el-table-column(label="详情",width="500")
           template(scope="scope")
-            span {{scope.row.updatedAt | moment}}
-        el-table-column(prop="description",label="详情",width="300")
+            span {{scope.row.description | ktNull}}
       el-pagination(@size-change="sizeChange",@current-change="currentChange",:current-page="parseInt(filter.page)",:page-sizes="page.sizes",:page-size="parseInt(filter.pageSize)",layout="total, sizes, prev, pager, next, jumper",:total="parseInt(page.total)")
 
 </template>
@@ -42,7 +51,7 @@ import {
 import {
   each
 } from 'lodash'
-export default{
+export default {
   data() {
     return {
       riskDatas: [],
@@ -59,16 +68,22 @@ export default{
         sizes: [10, 20, 30, 40]
       },
       options: [{
-        value: 'PASS',
-        label: '通过'
+        value: 'NEW',
+        label: '新预警'
       }, {
-        value: 'FAIL',
-        label: '未通过'
+        value: 'IGNORE',
+        label: '已忽略'
+      }, {
+        value: 'NOTIFIED',
+        label: '已通知'
+      }, {
+        value: 'PROCESSED',
+        label: '已处理'
       }]
     }
   },
   methods: {
-    risktemGet () {
+    risktemGet() {
       riskWarn.get({
         params: {
           ...pruneParams(this.filter)
@@ -78,8 +93,7 @@ export default{
         const data = res.data.data
         this.riskDatas = data.rows
         this.page.total = data.total
-      }).catch((res) => {
-      })
+      }).catch((res) => {})
     },
 
     clearFilter() {
@@ -113,23 +127,23 @@ export default{
       this.risktemGet()
     }
   },
-  mounted () {
+  mounted() {
     this.risktemGet()
   }
 }
 </script>
 
 <style lang="scss">
-  .icon-edit-delete{
-    i{
-      display: inline-block;
-      font-size:13px;
-      padding-right:10px;
-      text-align: center;
-    }
-    .icon-delete:hover{
-      cursor:pointer;
-      color:#538cc0
-    }
+.icon-edit-delete {
+  i {
+    display: inline-block;
+    font-size: 13px;
+    padding-right: 10px;
+    text-align: center;
   }
+  .icon-delete:hover {
+    cursor: pointer;
+    color: #538cc0
+  }
+}
 </style>
