cef82f6271c8e59a7ad3372574a84d1c61e3bd34
wensy <wensy@wjs.com>
2017-08-31 11:10:31 +0800

    资产详情页面，增加跳转至准入校验结果页面的链接

--- src/views/asset_manage/AssetInfoForm.vue
+++ src/views/asset_manage/AssetInfoForm.vue
@@ -3,6 +3,9 @@
   .box
     .box-header
       | 资产基础信息
+      .buttons
+        el-button(type="primary", size="small", @click="accessControl(assetBaseInfo.id)")
+          | 准入校验结果
     .box-content
       .box-section
         el-row(:gutter="20")
@@ -401,6 +404,12 @@ export default {
         this.assetCarBackUpInfo = data.assetCarBackUpInfo
         this.assetCarCoreInfo = data.assetCarCoreInfo
         this.assetInsuranceInfoList = data.assetInsuranceInfoList
+      })
+    },
+    accessControl(id) {
+      this.$router.push({
+        name: 'accessControl',
+        query: {subjectId: id}
       })
     }
   },
