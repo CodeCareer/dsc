6fb0326a79b63f6c06a9bf038fcc9cbe613a7ee3
洪城城 <hongcc@wjs.com>
2017-09-08 11:53:33 +0800

    还原修改

--- config/index.js
+++ config/index.js
@@ -31,21 +31,27 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-      '/api/fundManage': {
-        target: 'http://apidoc.wjs-dev.com/mockjsdata/61',
-        // target: 'http://192.168.220.217:8080',
-        changeOrigin: true,
-        pathRewrite: function(path, req) {
-          return path.replace('/api/', '/')
-        }
-      },
-      '/api/api/usermanage/v1/permission/listByRoleId': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        pathRewrite: function(path, req) {
-          return path.replace('/api/api/usermanage/v1/permission/listByRoleId', '/static/json/listByRoleId.json')
-        }
-      },
+      // '/api/thirdPartyData/vehicleManage': {
+      //   target: 'http://10.132.1.218:8080',
+      //   changeOrigin: true,
+      //   pathRewrite: function(path, req) {
+      //     return path.replace('/api/', '/')
+      //   }
+      // },
+      // '/api/thirdPartyData/gpsManage/': {
+      //   target: 'http://10.132.1.115:8080',
+      //   changeOrigin: true,
+      //   pathRewrite: function(path, req) {
+      //     return path.replace('/api/', '/')
+      //   }
+      // },
+      // '/api/api/usermanage/': {
+      //   target: 'http://10.132.1.171:9093',
+      //   changeOrigin: true,
+      //   pathRewrite: function(path, req) {
+      //     return path.replace('/api/api/', '/api/')
+      //   }
+      // },
       '/api': {
         target: 'https://kf-wdy.trunk.wjs-test.com/wdy',
         secure: false,
